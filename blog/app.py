8c77529baee4de7cc96622423e3b9128c4068ceb
aler33 <95293876+aler33@users.noreply.github.com>
2023-03-01 23:06:21 +0300

    Lesson 3 (#3)
    * Lesson_3 full

--- blog/app.py
+++ blog/app.py
@@ -2,10 +2,16 @@ from flask import Flask, request, g, render_template
 from time import time
 from blog.views.users import users_app
 from blog.views.articles import articles_app
+from blog.views.auth import login_manager, auth_app
+from blog.models.database import db
 
 
 app = Flask(__name__)
-
+# app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:////tmp/blog.db"
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:////home/alexey/gb_lesson/Flask_g/Flask_gb/blog/blog.db"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+app.config["SECRET_KEY"] = "abcdefg123456"
+db.init_app(app)
 
 @app.before_request
 def process_before_request():
@@ -17,6 +23,9 @@ def process_before_request():
 
 app.register_blueprint(users_app, url_prefix="/users")
 app.register_blueprint(articles_app, url_prefix="/articles")
+app.register_blueprint(auth_app, url_prefix="/auth")
+
+login_manager.init_app(app)
 
 
 @app.route("/")
@@ -33,3 +42,33 @@ def process_after_request(response):
         response.headers["process-time"] = time() - g.start_time
 
     return response
+
+
+@app.cli.command("init-db")
+def init_db():
+    """
+    Run in terminal:
+    flask init-db
+    """
+    db.create_all()
+
+
+@app.cli.command("create-users")
+def create_users():
+    """
+    Run in terminal:
+    flask create-users
+    """
+    from blog.models import User
+    admin = User(username='admin', is_staff=True, email='admin@localhost.ru')
+    james = User(username="James", email='james@localhost.ru')
+    brian = User(username="Brian", email='brian@localhost.ru')
+    peter = User(username="Peter", email='peter@localhost.ru')
+
+    db.session.add(admin)
+    db.session.add(james)
+    db.session.add(brian)
+    db.session.add(peter)
+    db.session.commit()
+
+    print("done! created users:", admin, james)
